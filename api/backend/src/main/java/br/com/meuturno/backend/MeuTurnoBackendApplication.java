cf0c537d7690c8614b172e8ae67d54ebc61c2601
Lucas-Teles <lucasteles.dev@gmail.com>
2025-08-05 22:15:42 -0300

    :tada: commit inicial

---
+++ api/backend/src/main/java/br/com/meuturno/backend/MeuTurnoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuturno.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTurnoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTurnoBackendApplication.class, args);
+	}
+
+}
---
+++ api/backend/src/test/java/br/com/meuturno/backend/MeuTurnoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuturno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuTurnoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
